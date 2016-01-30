4fb7d24b5a1152c444b8e60200b3690149af6685
lqCoder <apple@appledeMacBook-Pro-2.local>
2016-01-30 17:55:10 +0800

    repair Class

--- JsToClassFile/AppDelegate.h
+++ JsToClassFile/AppDelegate.h
@@ -1,10 +1,9 @@
 //
 //  AppDelegate.h
 //  JsToClassFile
-//
-//  Created by apple on 16/1/5.
+//  Created by li qiao on 16/1/5.
 //  Copyright © 2016年 apple. All rights reserved.
-//
+//  注意: 转成的类文件的建立格式与MJExtension第三方框架数据生成的格式相同，最好与MJExtension配套使用。我把转换的大体思路写出来了，我用的是MJExtension旧版本，MJExtension新版本的一些数据转换的地方变了，现在这个就不适用了。我就需要使用着看懂我的代码自己修改了。
 
 #import <Cocoa/Cocoa.h>
 
